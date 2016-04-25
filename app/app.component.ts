eeb458982bad0d09d24c391cfe10fa52f8dacfb2
Carlos Eduardo Torre <T802520@tokiomarine.com.br>
2016-04-25 18:00:38 -0300

    final do tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 import {DashboardComponent} from './dashboard.component';
 import {HeroesComponent} from './heroes.component';
@@ -10,14 +10,8 @@ import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a [routerLink]="['Dashboard']">Dashboard</a>
-      <a [routerLink]="['Heroes']">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [ROUTER_PROVIDERS, HeroService]
 })
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -8,6 +8,7 @@ import {HeroService} from './hero.service';
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html',
+  styleUrls:['app/dashboard.component.css'],
   directives: [DashboardComponent]
 })
 
